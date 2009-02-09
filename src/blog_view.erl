bdd32fb3059cb013b20f193de65a4d0368f54686
Ghalib <ghalib@sent.com>
2009-02-08 21:34:19 -0500

    Refactored blog_view.erl

--- src/blog_view.erl
+++ src/blog_view.erl
@@ -1,5 +1,5 @@
 -module(blog_view).
--author('ghalib <ghalib@sent.com').
+-author('ghalib <ghalib@sent.com>').
 
 -export([about_page/0]).
 
@@ -14,30 +14,29 @@ make_topbar(Links) ->
 
 about_text() -> 
     [{p, [], <<"Hello, my name is Ghalib Suleiman (or
-    Sulaiman). I graduated from university in December 2006.">>},
+    Sulaiman). I graduated from university in December 2006. Everyone
+    else seems to have a website, so I thought I would hop on the
+    bandwagon too.">>},
 
-     {p, [], <<"Everyone else seems to have a website, so I thought I
-     would hop on the bandwagon too .">>}].
+     {p, [], <<"If you are doing any interesting work, I would love to
+     hear from you. I can be reached at << ghalib ##at ##sent ^dot
+     #com# >>">>}].
 
-make_page(Content) ->
+make_page(Topbar_Links, Content) ->
     blog_util:html_text({html, [],
 			 [{head, [],
 			   [{link, [{rel, 'stylesheet'},
 				    {type, 'text/css'},
 				    {href, 'style.css'}], 
-			     []}]},s
+			     []}]},
 			  {body, [],
-			   [make_topbar([{<<"about">>,
-					  <<"About">>,
-					  "current"},
-					 {<<"code">>,
-					  <<"Code">>,
-					  ""},
-					 {<<"blog">>,
-					  <<"Blog">>,
-					  ""}]),
+			   [make_topbar(Topbar_Links),
 			    Content]}]}).
 
 about_page() ->
-    make_page({'div', [{id, 'text'}],
-	       asabout_text()}).
+    make_page([{<<"about">>, <<"About">>, "current"},
+	       {<<"code">>, <<"Code">>,	""},
+	       {<<"blog">>, <<"Blog">>,	""}],
+	      {'div', [{id, 'text'}],
+	       about_text()}).
+ 
