4dfbcaba7a8cc0f64c9d27dd6f7fab59dc186ee5
Ghalib <ghalib@sent.com>
2009-03-02 12:01:44 -0500

    Changed start.sh to reflect start-dev.sh in terms of choosing right mnesia dir

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s blog
+# exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s blog
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -sname mochi -boot start_sasl -s blog -mnesia dir '"db"'
