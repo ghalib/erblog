8ac6eb846133d6fb2579889012b30257a09add05
Ghalib <ghalib@sent.com>
2009-04-04 13:45:25 -0400

    Modified start.sh so that nodename is fully qualified

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 # exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s blog
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -sname mochi -boot start_sasl -s blog -mnesia dir '"db"'
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -name mochi -boot start_sasl -s blog -mnesia dir '"db"'
