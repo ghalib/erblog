4fdd7f3b4c90710e9729f3897b9485ddb6db7bdb
Ghalib <ghalib@sent.com>
2009-04-05 00:39:20 -0400

    Added readme and examplepost.el

---
+++ doc/examplepost.el
@@ -0,0 +1,7 @@
+(deftitle "An Intro to Reverse Induction")  ; title for this blogpost
+
+(defpara "My first blog post will be on baby (undergrad) maths. We have all seen the 'vanilla' induction (assuming you studied CS/maths) in undergrad, however I came across a neat variation on the idea. It is best illustrated with an example: Let's prove that ") ; a paragraph
+
+(defpara "another para with link: " (deflink "http://www.google.com" "A link to Google")) ; another paragraph with HTML link
+
+(defpara "para para para")
