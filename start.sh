a886f0a419c80d798b8898064f4240a571d89a4b
Ghalib <ghalib@sent.com>
2009-04-05 00:13:48 -0400

    Changed nodename in start.sh to erblog

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 # exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s blog
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -name mochi -boot start_sasl -s reloader -s blog -mnesia dir '"db"' -kernel inet_dist_listen_min 45000 inet_dist_listen_max 45005
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -name erblog -boot start_sasl -s reloader -s blog -mnesia dir '"db"' -kernel inet_dist_listen_min 45000 inet_dist_listen_max 45005
 
