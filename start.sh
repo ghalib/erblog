902df60be8b1e4811fcf234d702e383df3a735bf
Ghalib <ghalib@sent.com>
2009-04-04 20:40:29 -0400

    Changed start.sh such that a fully-qualified nodename is used, plus specific ports used for distributed Erlang. start.sh is how the 'official' server should be started (as opposed to start-dev.sh, which is only for development

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 # exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s blog
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -name mochi -boot start_sasl -s blog -mnesia dir '"db"'
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -name mochi -boot start_sasl -s reloader -s blog -mnesia dir '"db"' -kernel inet_dist_listen_min 45000 inet_dist_listen_max 45005
+
