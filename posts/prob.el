7d1e986b11634ccb0f4052d0de5d203cae138431
Ghalib Suleiman <ghalib@sent.com>
2010-12-23 23:28:11 -0500

    Added blogpost on probability book.

---
+++ posts/prob.el
@@ -0,0 +1,11 @@
+(deftitle "Probability Book Recommendation")
+
+(defpara "For those wanting to learn probability, I heartily
+recommend " (deflink "http://www.amazon.com" "DeGroot and
+Schervish") ".")
+
+(defpara "I sat in on a class that
+used " (deflink "http://www.amazon.com" "Ross") ", but (modulo
+the latter statistics half of M&S that Ross does not cover) I
+found the language in M&S to be more crisp and concise. I felt it
+was easier to find and extract the key points from it.")
