6083767ca55964744106e73223b625d6ee3cf074
Ghalib Suleiman <ghalib@sent.com>
2010-12-24 00:13:16 -0500

    Modification to probability book post; new post on cubicle screens.

---
+++ posts/cubescreens.el
@@ -0,0 +1,17 @@
+(deftitle "Projector/Display on the Inside of Cubicle Walls")
+
+(defpara "For most technical jobs where the employees reside
+in cubicles, the sight of coworkers gathering in someone's cube
+to look at their screen is a common occurrence. But said screen
+is usually running at high-resolution, optimised for viewing by
+one person: the one sitting in front of it.")
+
+(defpara "I wonder how practical it would be to somehow have a
+mirror display/projection on the inside of the cubicle wall,
+right at the entrance. This display would be switched on/off
+on-demand. Thus when people stop by the cubicle for a discussion,
+they can comfortably remain standing at the entrance and
+look/point at the mirror projection on the cubicle wall, instead
+of awkwardly bending and stretching their necks forward to get a
+closer look at the cubicle resident's screen")
+
--- posts/prob.el
+++ posts/prob.el
@@ -1,11 +1,11 @@
 (deftitle "Probability Book Recommendation")
 
-(defpara "For those wanting to learn probability, I heartily
-recommend " (deflink "http://www.amazon.com" "DeGroot and
-Schervish") ".")
+(defpara "I hope to resume blogging soon, but for now I wanted
+to briefly note that for those wanting to learn probability, I
+heartily recommend " (deflink "http://www.amazon.com" "DeGroot
+and Schervish") ".")
 
 (defpara "I sat in on a class that
 used " (deflink "http://www.amazon.com" "Ross") ", but (modulo
 the latter statistics half of M&S that Ross does not cover) I
-found the language in M&S to be more crisp and concise. I felt it
-was easier to find and extract the key points from it.")
+found the language in M&S to be more crisp and concise. I also found the key points easier to find and extract.")
