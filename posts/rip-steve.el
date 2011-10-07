504ceb409ec4de52f99e3276b2608981a320c338
Ghalib Suleiman <ghalib@sent.com>
2011-10-07 00:48:42 -0400

    Added RIP Steve post.

---
+++ posts/rip-steve.el
@@ -0,0 +1,26 @@
+(deftitle "RIP Steve")
+
+(defpara "About a year ago I emailed Steve Jobs asking for Mac OS
+X Lion to be distributed on USB sticks so that MacBook Air
+owners, lacking a disc drive, could easily install it.  He
+replied with a question: 'Why not electronic?'. Why not,
+indeed. As usual, Steve's mind was already 10 steps ahead; nine
+months later Lion was released electronically through the new Mac App
+Store.")
+
+(defpara "I laughed with my friends at how pedestrian I must have sounded. It
+was like telling a modern farmer that an ox is better than a horse for
+ploughing, only to have him ask 'Why not use a tractor?'.")
+
+(defpara "Steve's philosophy changed who I was for the positive: it affirmed
+that there is nothing weird about simultaneously caring for both form and function,
+that there is no shame in relentlessly striving for perfection. Most
+of all, I learned that I owe it to me to always be true to myself.")
+
+(defpara "Declaring someone a personal hero can sound hyperbolic, but in
+this case, I think it is warranted. Steve was one of my heroes.")
+
+(defpara "I'll end with my favourite video on the internet: " (deflink "http://www.youtube.com/watch?v=UF8uR6Z6KLc" "Steve's 2005
+Stanford commencement address") ".")
+
+(defpara "RIP Steve. I still have my original 5GB iPod.")
