dfc049c62812d7e3f3ae26f2061c63273d63ff17
Ghalib Suleiman <ghalib@sent.com>
2012-11-01 23:08:56 -0400

    Added post on apps future.

---
+++ posts/apps.el
@@ -0,0 +1,23 @@
+(deftitle "The Future of Apps Turned Out Different")
+
+(defpara "Six years ago, it was common to think that the future
+of apps lay in the browser. Most computers were anticipated to
+evolve into dumb terminals where the web browser would be the one
+window into applications. It seemed absurd to think otherwise.")
+
+(defpara "But the future turned out different. In addition to the
+browser-based apps we anticipated, we now have a marked
+resurgence in 'native' apps.")
+
+(defpara "What happened? Apple came out with their App Store,
+thereby solving the distribution problem. Previously, to download
+a native application, you had to hunt the web for an executable,
+download it, explicitly run it, then go through a series of
+installation steps only after which you could run the desired
+application.")
+
+(defpara "This leads me to wonder: just as HTTP and HTML are the
+ standard delivery and authorship standards for web apps, might
+ we one day see analogous standards that govern native apps?
+ Perhaps a necessary condition for this is multiple competing
+ vendors - something that isn't true today.")
