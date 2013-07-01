8959fd946906cd76a8000752459ca0aaf3b4bcc1
Ghalib Suleiman <ghalib@sent.com>
2013-06-30 22:11:47 -0400

    Added Three Books post

---
+++ posts/threebooks.el
@@ -0,0 +1,96 @@
+(deftitle "Three Introductory Textbooks I Hope to Read Someday")
+
+(defpara "I discovered these books right at the tail end of my mathematics education. I worked through some of their chapters but then stopped, and sadly have never gone back to them.")
+
+(ul
+ (li (header
+ 'h4 (deflink "http://www.amazon.com/dp/0914098918/" "Calculus") " by Spivak")
+     (defpara
+       "I can't find a better way to describe this textbook other
+than to say that it has been written with the utmost love. From
+the layout and the binding to the prose and exercises, the level
+of care that has gone into it is remarkable.")
+     (defpara "For what it is (an introductory undergrad book), some
+of the exercises can be quite challenging. But while reading it
+you can't help but feel that Spivak himself is there with you all
+the way, guiding you through things while knowing when to step
+back and let you figure it out for yourself (and if you find
+yourself truly stuck, he has generously created a comprehensive
+" (deflink "http://www.amazon.com/dp/0914098926" "solutions manual") ").")
+     (defpara "Far from aiming to only be a routine tutorial to
+calculus, the author's intention was for the book to also be a
+student's first introduction to higher mathematics. To that end,
+I think the closest analogy to it from the programming world
+is " (deflink "http://www.amazon.com/dp/0070004846" "SICP") ", so
+it may be fair to call this the 'SICP of mathematics' (except
+that it is grander, given that mathematics is a grander
+subject).")
+     (defpara "My undergrad calculus textbook
+was " (deflink "http://www.amazon.com/dp/0495011606" "Stewart") " (look
+at that eye-watering price). I know that it's an extremely
+popular choice but in contrast to Spivak I found it drab and
+dull, due to not doing much in the way of explaining the
+motivations or deep insights behind the introductions of various
+concepts."))
+
+ (li (header 'h4 (deflink "http://www.amazon.com/dp/0387982582" "Linear
+ Algebra Done Right") " by Axler")
+ (defpara "This book is clearly written by an opinionated
+person. The opinion in this case being that determinants are just
+not that important. And so, their introduction is left for right
+at the end of the book, after Jordan form.")
+ (defpara "It also eshews messy computation for what the
+author deems to be a cleaner exposition. Vector spaces and
+linear maps are introduced right at the beginning, and the rest
+of the book revolves around interesting definitions and proofs
+that follow from that. There is minimal talk of computation with
+matrices, thus it is in heavy contrast to a typical undergrad
+linear algebra class, where one dives into matrices and their
+computations rather arbitrarily.")
+ (defpara "All this is to say, this is certainly a book for
+the pure math majors (the author himself recommends it for a
+second course for such creatures). Do not read it if you want a
+quick real-world application (there are other books for that);
+instead do so for a deeper insight into the pretty abstractions
+underlying the subject."))
+
+ (li (header 'h4 (deflink "http://www.amazon.com/dp/0201558025/" "Concrete
+ Mathematics") " by Graham, Knuth, and Patashnik")
+
+     (defpara "I've never come across a serious mathematics textbook
+that at the same time has been written with such a sense of
+fun. This is immediately apparent when you crack it open - there
+are liner notes all throughout from students of the course
+this book was born from (one from the intro says \"I don't see
+how what I've learned will ever help me\").")
+
+     (defpara "What makes this book serious is of course the
+subject matter. Its aim is to introduce all the mathematics
+necessary for analysing algorithms (it would be good preparation
+for tackling the exercises in
+Knuth's " (deflink "http://www.amazon.com/dp/0321751043" "AoCP") "). The
+'concrete' in the title comes from the fact that it covers both
+continuous and discrete mathematics (though mostly discrete).")
+
+     (defpara "Having said that, the mathematics itself is
+clearly also treated with a fun spirit - concepts that you had
+previously assumed to be simple and superficial are probed deeply
+to reveal all sorts of mathematical 'sleight of hand' methods and
+manipulations. For example, chapter 3 is wholly devoted to the
+floor and ceiling functions. I did not know there was so much to
+say about these - when I was first taught them it was a simple
+definition and that was it; we all moved on rather quickly.")
+     (defpara "If you work through this book, you will come out
+of the other end an absolute ninja at algebraically manipulating
+mathematical formulae.")
+
+     (defpara "The font of this book, AMS Euler, deserves
+special mention (for which this book was a debut). To
+quote the book, the purpose of this font is 'to capture the
+flavor of mathematics as it might be written by a mathematician
+with excellent handwriting'. Seeing it on printed paper, I find
+the font very attractive indeed.")))
+
+(defpara "For now, the blunt truth is that I prioritise building things over
+working through these books. But there is a sincere (albeit perhaps unrealistic) wish to someday make time for them.")
+
