fa73c6073a9dded2a9827cc2db0ca7573b525502
Ghalib Suleiman <ghalib@sent.com>
2013-07-14 23:05:21 -0400

    Added post on Google Glass

---
+++ posts/glass.el
@@ -0,0 +1,40 @@
+(deftitle "Is Google Glass a Failed Product?")
+
+(defpara "If it's intended for the mass market, I would say yes. If you
+disagree, I think it's worth asking yourself: why aren't Segways and
+Bluetooth earpieces more popular?")
+
+(defpara "With any new product, two questions can be asked:")
+
+(defpara (defnumlist
+           "What need does it fulfill?"
+           "Is it the right product to do so?"))
+
+(defpara "Glass certainly makes it easier to access information compared to
+pulling out one's smartphone. By all accounts it does this rather well
+given its impressive technology.")
+
+(defpara "But I don't think it is the right product for normal people, for the
+simple reason that people look too dorky wearing it. This may
+make it better suited for niche environments where people aren't judged on
+their looks (e.g. certain occupations that would benefit from a
+heads-up display).")
+
+(defpara "I would be more convinced of mass market appeal if the product
+morphed such that it could be embedded into, say, sunglasses, without
+being noticeable (putting aside the question of whether society would
+ever accept a tech product that is both intrusive and invisible). But
+until then, its current form factor on people's faces is too much of
+an eyesore.")
+
+(defpara "One can point to all the early tech-enthusiast adopters
+of Glass as evidence of future success. It wouldn't be the first
+time tech people were enthusiastic about a product that quickly
+went mainstream (Macs running OS X were very much an early
+tech-crowd adoption). But tech-nerd enthusiasm isn't always
+a sufficient condition for future success; see 'Linux on the
+desktop' or initial " (deflink "http://slashdot.org/story/01/10/23/1816257/apple-releases-ipod" "iPod-scepticism") " for counterexamples.")
+
+(defpara "So while tech nerds revel with Glass, what will normal people do to
+alleviate the inconvenience of having to constantly pull out their
+smartphones? My bet is on smartwatches.")
