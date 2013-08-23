c309e3131ad63ad8df3f80789317ee58ff299c41
Ghalib Suleiman <ghalib@sent.com>
2013-08-23 00:23:45 -0400

    Added post on advice uselessness

---
+++ posts/advice.el
@@ -0,0 +1,35 @@
+(deftitle "Does Advice Have to be Useless?")
+
+(defpara "There is a large canon of expected mistakes from
+newbies in all areas: the first-time parent who panics when their
+baby cries; the freshly-graduated doctor who rushes through his
+first medical assessment; the newly-promoted manager who doesn't
+communicate with his employees; the newlywed who doesn't
+understand the concept of compromise; the list goes on. Simple
+mistakes that yield effective lessons for the future.")
+
+(defpara "All these people are commonly told \"Expect to screw up
+on your first time\". Yet the activities these first-timers
+undertake are not ones no one has performed before. In fact,
+countless individuals have done so - what we refer to as
+'experience'.")
+
+(defpara "Given the large body of past experience and its
+associated advice, why are newbies still expected to screw up?
+One can conclude that no one really expects said advice to be
+useful until _after_ the newbie has made the mistake.")
+
+(defpara "But does it have to be like this? Is there no way to learn and
+internalise common lessons and patterns without making mistakes? This
+way when the newbie is faced with a common pattern for the first time,
+the response becomes a simple case of applying the correct
+internalised lesson?")
+
+(defpara "A friend offered an idea akin to cognitive behavioural
+therapy: memorise whatever set of lessons you are intent on
+internalising, then test your recall every few months. The idea
+being that you eventually internalise them so deeply (akin to
+developing " (deflink "http://www.economist.com/news/science-and-technology/21582244-artificially-created-memories-can-be-just-strong-real-ones-la-recherche-du" "false
+memories") ") that you will recognise situations where they apply,
+even if you have not experienced them before. This sounds
+plausible, but neither of us has put it to the test.")
